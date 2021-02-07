76fa6ec6356a388a6ca9b5e7108661ccac1ffac5
Optik <optik.s2000@gmail.com>
2021-02-07 16:18:16 +0530

    change trial

--- src/components/sections/hero.js
+++ src/components/sections/hero.js
@@ -48,12 +48,12 @@ const Hero = () => {
   }, []);
 
   const one = <h1>Hi, my name is</h1>;
-  const two = <h2 className="big-heading">Brittany Chiang.</h2>;
-  const three = <h3 className="big-heading">I build things for the web.</h3>;
+  const two = <h2 className="big-heading">Sagar Sanjay Shenoy.</h2>;
+  const three = <h3 className="big-heading">I copy things from the web.</h3>;
   const four = (
     <p>
-      I'm a software engineer based in Boston, MA specializing in building (and occasionally
-      designing) exceptional websites, applications, and everything in between.
+      I'm a mechatronics engineer based in Manipal, Student specializing in building (and occasionally
+      designing) exceptional robots, applications, and everything in between.
     </p>
   );
   const five = (
