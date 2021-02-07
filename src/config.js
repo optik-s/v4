2a20ea302b33193713553a6c0c161d46d492c2c3
Optik <optik.s2000@gmail.com>
2021-02-07 16:07:28 +0530

    name mods

--- src/config.js
+++ src/config.js
@@ -4,7 +4,7 @@ module.exports = {
   socialMedia: [
     {
       name: 'GitHub',
-      url: 'https://github.com/bchiang7',
+      url: 'https://github.com/optik-s',
     },
     {
       name: 'Instagram',
@@ -16,7 +16,7 @@ module.exports = {
     },
     {
       name: 'Linkedin',
-      url: 'https://www.linkedin.com/in/bchiang7',
+      url: 'https://www.linkedin.com/in/sagar-sanjay-shenoy/',
     },
     {
       name: 'Codepen',
